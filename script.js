a7a28d402c9580fd1b7e8d50f4d79dde7e9ab1ff
Ivan Pavlyuk <ivanpavlyuk30071989@gmail.com>
2024-01-04 21:32:38 +0300

    Сделал акордеон для секции Команда

--- script.js
+++ script.js
@@ -21,4 +21,21 @@ function operateMenu() {
   );
 }
 
-// Команда
+// Команда(аккордеон)
+$(document).ready(function () {
+  const dropdowns = $('.person__info');
+  const buttons = $('.person__name');
+
+  dropdowns.slideUp();
+  buttons.click(function (e) {
+    let currentButton = $(e.currentTarget);
+    if (currentButton.hasClass('.person__name_active')) {
+      dropdowns.slideUp();
+      buttons.removeClass('.person__name_active');
+    } else {
+      dropdowns.slideUp();
+      currentButton.siblings('.person__info').slideDown();
+      currentButton.addClass('.person__name_active');
+    }
+  });
+});
