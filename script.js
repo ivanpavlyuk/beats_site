6ad2dfb777d8556f0fd0ccc2ea797d099cb8d58f
Ivan Pavlyuk <ivanpavlyuk30071989@gmail.com>
2024-01-07 10:55:12 +0300

    сделал пагинацию в секции Отзывы на чистом JS

--- script.js
+++ script.js
@@ -45,3 +45,20 @@ $(document).ready(function () {
   $('.slider').slick({
   });
 });
+
+// Пагинация в секции Отзывы
+{
+  const buttons = document.querySelectorAll('.feedbacks__button');
+  const cards = document.querySelectorAll('.feedbacks__card');
+
+  for (let index = 0; index < buttons.length; index++) {
+    const element = buttons[index];
+
+    element.addEventListener('click', function () {
+      cards.forEach(e => e.classList.remove('feedbacks__card_active'));
+      buttons.forEach(e => e.classList.remove('feedbacks__button_active'));
+      cards[index].classList.add('feedbacks__card_active');
+      element.classList.add('feedbacks__button_active');
+    })
+  }
+}
