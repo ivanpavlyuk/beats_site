33a32acfa6caa55d890f3e6f71c7c827a312d934
Ivan Pavlyuk <ivanpavlyuk30071989@gmail.com>
2024-01-18 19:30:05 +0300

    раскомментировал секции )

--- script.js
+++ script.js
@@ -1,341 +1,341 @@
-// // Бургер
-// const burgerButton = document.querySelector(".burger");
-// burgerButton.addEventListener("click", operateMenu);
+// Бургер
+const burgerButton = document.querySelector(".burger");
+burgerButton.addEventListener("click", operateMenu);
 
-// function operateMenu() {
-//   const menu = document.querySelector(".fullscreen-menu");
-//   menu.classList.add("fullscreen-menu_active");
+function operateMenu() {
+  const menu = document.querySelector(".fullscreen-menu");
+  menu.classList.add("fullscreen-menu_active");
 
-//   const closeButton = document.querySelector(".fullscreen-menu__button");
-//   closeButton.addEventListener("click", () => {
-//     document
-//       .querySelector(".fullscreen-menu")
-//       .classList.remove("fullscreen-menu_active");
-//   });
+  const closeButton = document.querySelector(".fullscreen-menu__button");
+  closeButton.addEventListener("click", () => {
+    document
+      .querySelector(".fullscreen-menu")
+      .classList.remove("fullscreen-menu_active");
+  });
 
-//   const menuItems = menu.querySelectorAll(".menu__item");
-//   menuItems.forEach((item) =>
-//     item.addEventListener("click", () => {
-//       menu.classList.remove("fullscreen-menu_active");
-//     })
-//   );
-// }
+  const menuItems = menu.querySelectorAll(".menu__item");
+  menuItems.forEach((item) =>
+    item.addEventListener("click", () => {
+      menu.classList.remove("fullscreen-menu_active");
+    })
+  );
+}
 
-// // Команда(аккордеон)
-// $(document).ready(function () {
-//   const dropdowns = $('.person__info');
-//   const buttons = $('.person__name');
+// Команда(аккордеон)
+$(document).ready(function () {
+  const dropdowns = $('.person__info');
+  const buttons = $('.person__name');
 
-//   dropdowns.slideUp();
-//   buttons.click(function (e) {
-//     let currentButton = $(e.currentTarget);
-//     if (currentButton.hasClass('person__name_active')) {
-//       dropdowns.slideUp();
-//       buttons.removeClass('person__name_active');
-//     } else {
-//       dropdowns.slideUp();
-//       currentButton.addClass('person__name_active');
-//       currentButton.siblings('.person__info').slideDown();
-//     }
-//   });
-// });
+  dropdowns.slideUp();
+  buttons.click(function (e) {
+    let currentButton = $(e.currentTarget);
+    if (currentButton.hasClass('person__name_active')) {
+      dropdowns.slideUp();
+      buttons.removeClass('person__name_active');
+    } else {
+      dropdowns.slideUp();
+      currentButton.addClass('person__name_active');
+      currentButton.siblings('.person__info').slideDown();
+    }
+  });
+});
 
-// // Слайдер на плагине slick
-// $(document).ready(function () {
-//   $('.slider').slick({
-//   });
-// });
+// Слайдер на плагине slick
+$(document).ready(function () {
+  $('.slider').slick({
+  });
+});
 
-// // Пагинация в секции Отзывы
-// {
-//   const buttons = document.querySelectorAll('.feedbacks__button');
-//   const cards = document.querySelectorAll('.feedbacks__card');
+// Пагинация в секции Отзывы
+{
+  const buttons = document.querySelectorAll('.feedbacks__button');
+  const cards = document.querySelectorAll('.feedbacks__card');
 
-//   for (let index = 0; index < buttons.length; index++) {
-//     const element = buttons[index];
+  for (let index = 0; index < buttons.length; index++) {
+    const element = buttons[index];
 
-//     element.addEventListener('click', function () {
-//       cards.forEach(e => e.classList.remove('feedbacks__card_active'));
-//       buttons.forEach(e => e.classList.remove('feedbacks__button_active'));
-//       cards[index].classList.add('feedbacks__card_active');
-//       element.classList.add('feedbacks__button_active');
-//     })
-//   }
-// }
+    element.addEventListener('click', function () {
+      cards.forEach(e => e.classList.remove('feedbacks__card_active'));
+      buttons.forEach(e => e.classList.remove('feedbacks__button_active'));
+      cards[index].classList.add('feedbacks__card_active');
+      element.classList.add('feedbacks__button_active');
+    })
+  }
+}
 
-// // Форма
-// $('.form').submit(function (e) {
-//   e.preventDefault();
+// Форма
+$('.form').submit(function (e) {
+  e.preventDefault();
 
-//   const form = $(e.currentTarget);
-//   const name = form.find('[name="name"]');
-//   const phone = form.find('[name="phone"]');
-//   const comment = form.find('[name="comment"]');
-//   const to = form.find('[name="to"]');
+  const form = $(e.currentTarget);
+  const name = form.find('[name="name"]');
+  const phone = form.find('[name="phone"]');
+  const comment = form.find('[name="comment"]');
+  const to = form.find('[name="to"]');
 
-//   [phone, name, comment, to].forEach(field => {
-//     field.removeClass('form__input-error');
-//     if (field.val().trim() === '') {
-//       field.addClass('form__input-error');
-//     }
-//   });
+  [phone, name, comment, to].forEach(field => {
+    field.removeClass('form__input-error');
+    if (field.val().trim() === '') {
+      field.addClass('form__input-error');
+    }
+  });
 
-//   const modal = $('#modal');
-//   const message = modal.find('.delivery__message');
-//   message.css('color', 'black');
+  const modal = $('#modal');
+  const message = modal.find('.delivery__message');
+  message.css('color', 'black');
 
-//   if (form.find('.form__input-error').length === 0) {
-//     $.ajax({
-//       type: "post",
-//       url: "https://webdev-api.loftschool.com/sendmail",
-//       data: {
-//         name: name.val(),
-//         phone: phone.val(),
-//         comment: comment.val(),
-//         to: to.val()
-//       },
-//       success: data => {
-//         message.text(data.message);
-//         Fancybox.show([{
-//           src: '#modal',
-//           type: 'inline'
-//         }]);
-//       },
-//       error: data => {
-//         const response = data.responseJSON.message;
-//         message.text(response);
-//         message.css('color', 'red');
-//         Fancybox.show([{
-//           src: '#modal',
-//           type: 'inline'
-//         }]);
+  if (form.find('.form__input-error').length === 0) {
+    $.ajax({
+      type: "post",
+      url: "https://webdev-api.loftschool.com/sendmail",
+      data: {
+        name: name.val(),
+        phone: phone.val(),
+        comment: comment.val(),
+        to: to.val()
+      },
+      success: data => {
+        message.text(data.message);
+        Fancybox.show([{
+          src: '#modal',
+          type: 'inline'
+        }]);
+      },
+      error: data => {
+        const response = data.responseJSON.message;
+        message.text(response);
+        message.css('color', 'red');
+        Fancybox.show([{
+          src: '#modal',
+          type: 'inline'
+        }]);
 
-//       }
-//     });
-//   }
+      }
+    });
+  }
 
-//   $('.delivery__button').click(function (e) {
-//     e.preventDefault();
-//     Fancybox.close();
-//   });
-// });
+  $('.delivery__button').click(function (e) {
+    e.preventDefault();
+    Fancybox.close();
+  });
+});
 
-// // Горизонтальный аккордеон
-// function showAccordeon() {
-//   action();
+// Горизонтальный аккордеон
+function showAccordeon() {
+  action();
 
-//   function action() {
-//     const buttons = document.querySelectorAll('.colors__item-title');
-//     const blocks = document.querySelectorAll('.colors__item-content');
-//     const texts = document.querySelectorAll('.colors__item-text');
+  function action() {
+    const buttons = document.querySelectorAll('.colors__item-title');
+    const blocks = document.querySelectorAll('.colors__item-content');
+    const texts = document.querySelectorAll('.colors__item-text');
 
-//     let buttonsWidth = 0;
-//     let blockWidth = 0;
+    let buttonsWidth = 0;
+    let blockWidth = 0;
 
-//     function closeButtons() {
-//       buttons.forEach(button => {
-//         button.isClosed = true;
-//         button.nextElementSibling.style.width = '0px';
-//         buttonsWidth = 0;
-//         blockWidth = 0;
-//       });
-//     }
+    function closeButtons() {
+      buttons.forEach(button => {
+        button.isClosed = true;
+        button.nextElementSibling.style.width = '0px';
+        buttonsWidth = 0;
+        blockWidth = 0;
+      });
+    }
 
-//     function closeButtonsOutside() {
-//       document.addEventListener('click', (e) => {
-//         let withinBoundaries = false;
-//         buttons.forEach(button => {
-//           withinBoundaries = withinBoundaries || e.composedPath().includes(button);
-//         });
+    function closeButtonsOutside() {
+      document.addEventListener('click', (e) => {
+        let withinBoundaries = false;
+        buttons.forEach(button => {
+          withinBoundaries = withinBoundaries || e.composedPath().includes(button);
+        });
 
-//         if (!withinBoundaries) {
-//           closeButtons();
-//         }
-//       })
-//     }
+        if (!withinBoundaries) {
+          closeButtons();
+        }
+      })
+    }
 
-//     closeButtonsOutside();
-//     closeButtons();
+    closeButtonsOutside();
+    closeButtons();
 
-//     buttons.forEach(button => {
-//       button.addEventListener('click', e => {
-//         if (window.innerWidth < 830) {
-//           // Calculate dimensions
-//           buttons.forEach(button => {
-//             buttonsWidth += button.offsetWidth;
-//           });
-//           let textWidth = window.innerWidth - buttonsWidth;
-//           buttons.forEach(button => {
-//             button.nextElementSibling.querySelector('.colors__item-text').style.width = textWidth + 'px';
-//           });
+    buttons.forEach(button => {
+      button.addEventListener('click', e => {
+        if (window.innerWidth < 830) {
+          // Calculate dimensions
+          buttons.forEach(button => {
+            buttonsWidth += button.offsetWidth;
+          });
+          let textWidth = window.innerWidth - buttonsWidth;
+          buttons.forEach(button => {
+            button.nextElementSibling.querySelector('.colors__item-text').style.width = textWidth + 'px';
+          });
 
-//           if (button.isClosed) {
-//             closeButtons();
-//             button.nextElementSibling.style.width = textWidth + 'px';
-//             button.isClosed = false;
-//           } else {
-//             button.nextElementSibling.style.width = '0px';
-//             closeButtons();
-//           }
-//         } else {
-//           if (button.isClosed) {
-//             closeButtons();
-//             button.nextElementSibling.style.width = '524px';
-//             button.nextElementSibling.querySelector('.colors__item-text').style.width = '524px';
-//             button.isClosed = false;
-//           } else {
-//             button.nextElementSibling.style.width = '0px';
-//             closeButtons();
-//           }
-//         }
-//       })
+          if (button.isClosed) {
+            closeButtons();
+            button.nextElementSibling.style.width = textWidth + 'px';
+            button.isClosed = false;
+          } else {
+            button.nextElementSibling.style.width = '0px';
+            closeButtons();
+          }
+        } else {
+          if (button.isClosed) {
+            closeButtons();
+            button.nextElementSibling.style.width = '524px';
+            button.nextElementSibling.querySelector('.colors__item-text').style.width = '524px';
+            button.isClosed = false;
+          } else {
+            button.nextElementSibling.style.width = '0px';
+            closeButtons();
+          }
+        }
+      })
 
-//     });
-//   }
-// }
-// showAccordeon();
+    });
+  }
+}
+showAccordeon();
 
-// // Прокрутка страницы
+// Прокрутка страницы
 
-// window.addEventListener('load', e => {
+window.addEventListener('load', e => {
 
-//   const page = document.querySelector('.wrapper__main-content');
-//   const sections = document.querySelectorAll('.section');
-//   const links = document.querySelectorAll('.menu__link');
-//   const dots = document.querySelectorAll('.fixed-paginator__button');
+  const page = document.querySelector('.wrapper__main-content');
+  const sections = document.querySelectorAll('.section');
+  const links = document.querySelectorAll('.menu__link');
+  const dots = document.querySelectorAll('.fixed-paginator__button');
 
-//   let counter = 0;
+  let counter = 0;
 
-//   function validateCounter() {
-//     return counter >= 0 && counter < sections.length;
-//   }
+  function validateCounter() {
+    return counter >= 0 && counter < sections.length;
+  }
 
-//   function makePaginationActive(counter) {
-//     dots.forEach(dot => {
-//       dot.classList.remove('fixed-paginator__button_active');
-//     });
-//     dots[counter].classList.add('fixed-paginator__button_active');
-//   }
+  function makePaginationActive(counter) {
+    dots.forEach(dot => {
+      dot.classList.remove('fixed-paginator__button_active');
+    });
+    dots[counter].classList.add('fixed-paginator__button_active');
+  }
 
-//   function scroll(index) {
-//     counter = index;
-//     page.style.marginTop = -100 * counter + 'vh';
-//     makePaginationActive(counter);
-//   }
+  function scroll(index) {
+    counter = index;
+    page.style.marginTop = -100 * counter + 'vh';
+    makePaginationActive(counter);
+  }
 
-//   function scrollUp() {
-//     if (validateCounter()) {
-//       counter = counter > 0 ? --counter : counter;
-//       page.style.marginTop = -100 * counter + 'vh';
-//       makePaginationActive(counter);
-//     }
-//   }
+  function scrollUp() {
+    if (validateCounter()) {
+      counter = counter > 0 ? --counter : counter;
+      page.style.marginTop = -100 * counter + 'vh';
+      makePaginationActive(counter);
+    }
+  }
 
-//   function scrollDown() {
-//     if (validateCounter()) {
-//       counter = counter === sections.length - 1 ? counter : ++counter;
-//       page.style.marginTop = -100 * counter + 'vh';
-//       makePaginationActive(counter);
-//     }
-//   }
+  function scrollDown() {
+    if (validateCounter()) {
+      counter = counter === sections.length - 1 ? counter : ++counter;
+      page.style.marginTop = -100 * counter + 'vh';
+      makePaginationActive(counter);
+    }
+  }
 
-//   links.forEach(link => {
-//     link.addEventListener('click', e => {
-//       e.preventDefault();
-//       sections.forEach((section, index) => {
-//         if (e.target.getAttribute('href') === '#' + section.getAttribute('id')) {
-//           scroll(index);
-//         }
-//       });
-//     })
-//   });
+  links.forEach(link => {
+    link.addEventListener('click', e => {
+      e.preventDefault();
+      sections.forEach((section, index) => {
+        if (e.target.getAttribute('href') === '#' + section.getAttribute('id')) {
+          scroll(index);
+        }
+      });
+    })
+  });
 
-//   window.addEventListener('wheel', e => {
-//     if (e.deltaY < 0) {
-//       scrollUp();
-//     } else {
-//       scrollDown();
-//     }
-//   })
+  window.addEventListener('wheel', e => {
+    if (e.deltaY < 0) {
+      scrollUp();
+    } else {
+      scrollDown();
+    }
+  })
 
-//   dots.forEach((dot, index) => {
-//     dot.addEventListener('click', e => {
-//       scroll(index);
-//     })
-//   });
+  dots.forEach((dot, index) => {
+    dot.addEventListener('click', e => {
+      scroll(index);
+    })
+  });
 
-//   window.addEventListener('keydown', e => {
-//     if (e.key === 'ArrowDown') {
-//       scrollDown();
-//     } else if (e.key === 'ArrowUp') {
-//       scrollUp();
-//     }
-//   });
-// })
+  window.addEventListener('keydown', e => {
+    if (e.key === 'ArrowDown') {
+      scrollDown();
+    } else if (e.key === 'ArrowUp') {
+      scrollUp();
+    }
+  });
+})
 
 
 
-// // Скролл для телефонов
-// window.addEventListener('load', () => {
-//   const page = document.querySelector('.wrapper__main-content');
-//   const sections = document.querySelectorAll('.section');
-//   let counter = 0;
+// Скролл для телефонов
+window.addEventListener('load', () => {
+  const page = document.querySelector('.wrapper__main-content');
+  const sections = document.querySelectorAll('.section');
+  let counter = 0;
 
-//   let startY = 0;
+  let startY = 0;
 
-//   window.addEventListener('touchstart', (e) => {
-//     startY = e.touches[0].clientY;
-//   });
+  window.addEventListener('touchstart', (e) => {
+    startY = e.touches[0].clientY;
+  });
 
-//   window.addEventListener('touchmove', (e) => {
-//     const deltaY = e.touches[0].clientY - startY;
+  window.addEventListener('touchmove', (e) => {
+    const deltaY = e.touches[0].clientY - startY;
 
-//     if (deltaY > 100) { // You can adjust this threshold
-//       counter = counter > 0 ? --counter : counter;
-//       page.style.marginTop = -100 * counter + 'vh';
-//       startY = e.touches[0].clientY;
-//     } else if (deltaY < -100) { // You can adjust this threshold
-//       counter = counter === sections.length - 1 ? counter : ++counter;
-//       page.style.marginTop = -100 * counter + 'vh';
-//       startY = e.touches[0].clientY;
+    if (deltaY > 100) { // You can adjust this threshold
+      counter = counter > 0 ? --counter : counter;
+      page.style.marginTop = -100 * counter + 'vh';
+      startY = e.touches[0].clientY;
+    } else if (deltaY < -100) { // You can adjust this threshold
+      counter = counter === sections.length - 1 ? counter : ++counter;
+      page.style.marginTop = -100 * counter + 'vh';
+      startY = e.touches[0].clientY;
 
-//     }
-//   });
-// });
+    }
+  });
+});
 
-// // Карта
-// let myMap;
-// const init = () => {
-//   myMap = new ymaps.Map("map", {
-//     center: [59.93916998692174, 30.309015096732622],
-//     zoom: 11,
-//     controls: [],
-//   });
+// Карта
+let myMap;
+const init = () => {
+  myMap = new ymaps.Map("map", {
+    center: [59.93916998692174, 30.309015096732622],
+    zoom: 11,
+    controls: [],
+  });
 
-//   let coords = [
-//     [59.94554327989287, 30.38935262114668],
-//     [59.91142323563909, 30.50024587065841],
-//     [59.88693161784606, 30.319658102103713],
-//     [59.97033574821672, 30.315194906302924],
-//   ],
-//     myCollection = new ymaps.GeoObjectCollection({}, {
-//       draggable: false,
-//       iconLayout: 'default#image',
-//       iconImageHref: './img/icons/marker.svg',
-//       iconImageSize: [46, 57],
-//       iconImageOffset: [-35, -52]
-//     });
+  let coords = [
+    [59.94554327989287, 30.38935262114668],
+    [59.91142323563909, 30.50024587065841],
+    [59.88693161784606, 30.319658102103713],
+    [59.97033574821672, 30.315194906302924],
+  ],
+    myCollection = new ymaps.GeoObjectCollection({}, {
+      draggable: false,
+      iconLayout: 'default#image',
+      iconImageHref: './img/icons/marker.svg',
+      iconImageSize: [46, 57],
+      iconImageOffset: [-35, -52]
+    });
 
-//   for (let i = 0; i < coords.length; i++) {
-//     myCollection.add(new ymaps.Placemark(coords[i]));
-//   }
+  for (let i = 0; i < coords.length; i++) {
+    myCollection.add(new ymaps.Placemark(coords[i]));
+  }
 
-//   myMap.geoObjects.add(myCollection);
+  myMap.geoObjects.add(myCollection);
 
-//   myMap.behaviors.disable('scrollZoom');
-// };
+  myMap.behaviors.disable('scrollZoom');
+};
 
-// ymaps.ready(init);
+ymaps.ready(init);
 
 // Плеер
 
